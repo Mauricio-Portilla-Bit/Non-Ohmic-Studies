c7b371db0e6672f0f70e8c17ac2694116f038561
Mauricio Portilla <portillarmz@gmail.com>
2023-03-15 23:16:17 -0600

    [Add] Graph and Data process pipeline

--- SampleAnalysis.py
+++ SampleAnalysis.py
@@ -2,10 +2,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 # Función para estructurar los datos recibidos de txt a un dataframe
-def StructureData():
+def structure_data():
     print("Clean Data")
+    return 0
+
 
 # Función para analizar eléctricamente una determinada muestra, ajustando los datos
-def SampleAnalysis():
+def sample_analysis():
     print("Sample Analysis")
+
+
+data = {"E": [], "J": []}
+
+with open('Pruebas_electricas_1.txt') as f:
+    for line in f.readlines():
+        line_array = line.split()
+        for i in range(len(line_array)):
+
+            if i == 1:
+                data["J"].append(float(line_array[i]))
+
+            if i == 0:
+                data["E"].append(float(line_array[i]))
+
+
+df = pd.DataFrame(data)
+
+plt.plot(df["E"], df["J"])
+plt.scatter(df["E"], df["J"], c="r")
+plt.grid()
+plt.title("CAMPO ELÉCTRICO CONTRA DENSIDAD DE CORRIENTE")
+plt.xlabel("CAMPO ELÉCTRICO (V/cm)")
+plt.ylabel("DENSIDAD DE CORRIENTE (A/cm^2)")
+plt.show()
