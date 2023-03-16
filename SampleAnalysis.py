a72d2e656ad465eb7f83defd459ba3afd1553076
Mauricio Portilla <portillarmz@gmail.com>
2023-03-15 23:21:11 -0600

    [Structure] Code in modules

--- SampleAnalysis.py
+++ SampleAnalysis.py
@@ -4,36 +4,42 @@ import numpy as np
 
 
 # Función para estructurar los datos recibidos de txt a un dataframe
-def structure_data():
-    print("Clean Data")
-    return 0
+def structure_data(file):
+    data = {"E": [], "J": []}
+
+    with open(file) as f:
+        for line in f.readlines():
+            line_array = line.split()
+            for i in range(len(line_array)):
+
+                if i == 1:
+                    data["J"].append(float(line_array[i]))
+
+                if i == 0:
+                    data["E"].append(float(line_array[i]))
+
+    return pd.DataFrame(data)
 
 
-# Función para analizar eléctricamente una determinada muestra, ajustando los datos
+# Función para analizar eléctricamente una determinada muestra, ajustando los datos al modelo
 def sample_analysis():
     print("Sample Analysis")
 
 
-data = {"E": [], "J": []}
-
-with open('Pruebas_electricas_1.txt') as f:
-    for line in f.readlines():
-        line_array = line.split()
-        for i in range(len(line_array)):
-
-            if i == 1:
-                data["J"].append(float(line_array[i]))
-
-            if i == 0:
-                data["E"].append(float(line_array[i]))
+# Función para graficar la data que se recibe
+def graph_data(df):
+    plt.plot(df["E"], df["J"])
+    plt.scatter(df["E"], df["J"], c="r")
+    plt.grid()
+    plt.title("CAMPO ELÉCTRICO CONTRA DENSIDAD DE CORRIENTE")
+    plt.xlabel("CAMPO ELÉCTRICO (V/cm)")
+    plt.ylabel("DENSIDAD DE CORRIENTE (A/cm^2)")
+    plt.show()
 
 
-df = pd.DataFrame(data)
+file = 'Pruebas_electricas_1.txt'
+df = structure_data(file)
+graph_data(df)
 
-plt.plot(df["E"], df["J"])
-plt.scatter(df["E"], df["J"], c="r")
-plt.grid()
-plt.title("CAMPO ELÉCTRICO CONTRA DENSIDAD DE CORRIENTE")
-plt.xlabel("CAMPO ELÉCTRICO (V/cm)")
-plt.ylabel("DENSIDAD DE CORRIENTE (A/cm^2)")
-plt.show()
+
+
