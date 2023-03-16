362f48a6acd2e8e258c06c603b0af89bd7e1cd26
Mauricio Portilla <portillarmz@gmail.com>
2023-03-15 21:45:52 -0600

    [Add] Definition of Functions

---
+++ SampleAnalysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Función para estructurar los datos recibidos de txt a un dataframe
+def StructureData():
+    print("Clean Data")
+
+# Función para analizar eléctricamente una determinada muestra, ajustando los datos
+def SampleAnalysis():
+    print("Sample Analysis")
